2ab6e26aa9855107bad40e2e88d6a5a74ae38c21
diafr <diafreak212@gmail.com>
2021-02-14 22:32:26 +0100

    made cart close-able

--- views/shoppingCart.php
+++ views/shoppingCart.php
@@ -1,31 +1,38 @@
-<!--________________WARENKORB________________-->
+
 <div class="warenkorbcontainer">
     <aside class="warenkorb aktiv" id="w">
+
         <div class="warenkorb-header">
-            <a href="#">></a>
-            <p>Meine Einkaufsliste</p>
+            <a href=<?= getUrlWithoutCart() ?>>X</a>
+            <p>Mein Warenkorb</p>
         </div>
+
         <div class="warenkorbliste"> <!-- hier wurde noch was mit data load gemacht um dann vermutlich die daten auszulesen -->
             <div class="warenkorbinhalt">
                 <table class="warenreihe">
                     <tbody>
                         <tr> <!-- jede Reihe ist ein Produkt im Warenkorb mit: -->
+
                             <td> <!-- Bild -->
                                 <a href="?c=shop&a=productDetails" type="no-hover">
                                     <img src=<?=IMAGESPATH . 'placeholder.png'?> alt="Frucht" width="25px" height="25px">
                                 </a>
                             </td>
+
                             <td> <!-- Name & Anzahl -->
                                 <h3 class="produktname">Eine Frucht!</h3>
                                 <p>14125 Tonnen</p>
                             </td>
-                            <td> <!-- Preis -->
 
+                            <td> <!-- Preis -->
+                                3 Geld
                             </td>
+
                         </tr>
                     </tbody>
                 </table>
             </div>
         </div>
+
     </aside>
 </div>
