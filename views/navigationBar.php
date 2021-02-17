3d98546352760a7fb3ea74111f39befbf960cb82
diafr <diafreak212@gmail.com>
2021-02-17 20:00:30 +0100

    fixed fruits&vegetables

--- views/navigationBar.php
+++ views/navigationBar.php
@@ -11,30 +11,33 @@
                 <a href="?c=pages&a=home">
                     <li>HOME</li>
                 </a>
-                <ul class="dropdown-menu">
-                    <a href="?c=shop&a=fruits">
+
+                <a href="?c=shop&a=fruits">
+                    <ul class="dropdown-menu">
                         <li>OBST</li>
-                    </a>
-                    <div class="dropdown-content">
+                    <!-- <div class="dropdown-content">
                         <a href="irgendwas:)">Kernobst</a>
                         <a href="irgendwas:)">Steinobst</a>
                         <a href="irgendwas:)">Beeren</a>
                         <a href="irgendwas:)">Zitrusfrüchte</a>
                         <a href="irgendwas:)">Südfrüchte</a>
-                    </div>
-                </ul>
-                <ul class="dropdown-menu">
-                    <a href="?c=shop&a=vegetables">
+                    </div> -->
+                    </ul>
+                </a>
+
+                <a href="?c=shop&a=vegetables">
+                    <ul class="dropdown-menu">
                         <li>GEMÜSE</li>
-                    </a>
-                    <div class="dropdown-content">
+                    <!-- <div class="dropdown-content">
                         <a href="irgendwas:)">Fruchtgemüse</a>
                         <a href="irgendwas:)">(Blatt)Salat</a>
                         <a href="irgendwas:)">Kohlgemüse</a>
                         <a href="irgendwas:)">Knollengemüse</a>
                         <a href="irgendwas:)">Zwiebelgewächse</a>
-                    </div>
-                </ul>
+                    </div> -->
+                    </ul>
+                </a>
+
                 <a href="?c=pages&a=current">
                     <li>AKTUELLES</li>
                 </a>
