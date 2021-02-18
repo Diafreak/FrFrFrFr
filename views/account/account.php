769be93c820cf932cb0c28a7d82ba9461c073384
DerNoobzockt <leonmause@gmail.com>
2021-02-18 19:06:08 +0100

    Grundigste Grundzüge Account Seite

--- views/account/account.php
+++ views/account/account.php
@@ -1,6 +1,23 @@
 <h1><?=$test?></h1>
 
+<img src=<?=IMAGESPATH . 'user-placeholder.png'?> alt="User-Picture" class="user-picture">
+
 <form method="post">
-    <br><br>
+    <br>
     <input type="submit" name="submitLogout" value="Logout">
+</form>
+<br>
+<form method="post">
+        <input type="string" name="changeEmail" id="chemail" placeholder="E-Mail Adresse ändern" autocapitalize="off">
+        <input type="submit" name="submitEmailChange" value="Change">
+</form>
+<br>
+<form method="post">
+        <input type="password" name="changePassword" id="chpassword" placeholder="Passwort ändern" autocapitalize="off">
+        <input type="submit" name="submitPasswordChange" value="Change">
+</form>
+<br>
+<form method="post">
+        <input type="string" name="address" id="address" placeholder="Addresse hinzufügen / ändern" autocapitalize="off">
+        <input type="submit" name="submitAddress" value="Submit">
 </form>
