f6d8d06e921147875a8d019cc1150b6f27e6626f
diafr <diafreak212@gmail.com>
2021-02-20 17:00:01 +0100

    Whitespace

--- views/shop/productDetails.php
+++ views/shop/productDetails.php
@@ -1,6 +1,7 @@
 
 <!-- Errors -->
 <? if (isset($errors)) : ?>
+
     <div style="text-align: center;">
         <? foreach ($errors as $error) : ?>
             <?= $error ?>
@@ -11,33 +12,36 @@
 
     <h1><?= $name; ?></h1>
 
-<div class="detailwrapper">
+    <div class="detailwrapper">
+
     <div class="pdetailspic">
         <img src=<?= $imageUrl ?> alt=<?= $altText ?> width='250em' height='250em'>
     </div>
 
     <div class="pdetailsdesc">
-    Beschreibung: 
-    <br><?= $description; ?>
+        Beschreibung: 
+        <br><?= $description; ?>
     </div>
 
-    
     <div class="pdetailskauf">
-    Preis: <?= $price; ?>€
-    <br>
+        Preis: <?= $price; ?>€
+        <br>
 
-    Auf Lager: <?= $numberInStock; ?>
-    <br>
-    <form method="post">
-        <div class="menge_mit_submit">
-            <div class="mengenauswahl">
-                <?= generateAmountHTML($numberInStock) ?>
-                <!-- <p>oder ander Menge wählen</p>
-                und dann hier noch ein Formular wo man eine eigene Menge reinschreiben kann -->
+        Auf Lager: <?= $numberInStock; ?>
+        <br>
+
+        <form method="post">
+            <div class="menge_mit_submit">
+                <div class="mengenauswahl">
+                    <?= generateAmountHTML($numberInStock) ?>
+                </div>
+                <!-- "In den Warenkorb"-Button -->
+                <button type="submit" class="submit_amount" name="submitProduct" <?= ($numberInStock <= 0) ? "disabled" : "" ?>>In den Warenkorb</button>
             </div>
-            <button type="submit" class="submit_amount" name="submitProduct" <?= ($numberInStock <= 0) ? "disabled" : "" ?>>In den Warenkorb</button>
-        </div>
-    </form>
+        </form>
+
     </div>
+
 </div>
+
 <? endif; ?><br>
