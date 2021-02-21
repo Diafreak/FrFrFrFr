47b56cc0e63f67e84f5e21d47471380faafbc08f
diafr <diafreak212@gmail.com>
2021-02-21 21:24:41 +0100

    removed old actions

--- controller/pages_controller.php
+++ controller/pages_controller.php
@@ -3,27 +3,7 @@
 
 class PagesController extends Controller
 {
-
-    public function actionHome()
-    {
-        if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] === true) /*Brauchen wir das überhaupt noch?*/
-        {
-            $test = "Home - Eingeloggt!";
-        }
-        else
-        {
-            $test = "HOME - Nicht eingeloggt :(";
-        }
-
-        $this->setParam('test', $test);
-    }
-
-
-    public function actionAbout()
-    {
-        $test = "ÜBER UNS";
-        $this->setParam('test', $test);
-    }
+    public function actionHome(){}
 
 
     public function actionContact()
@@ -38,14 +18,6 @@ class PagesController extends Controller
         $test = "AKTUELLES";
         $this->setParam('test', $test);
     }
-
-
-    public function actionProducts()
-    {
-        $test = "PRODUKTE";
-        $this->setParam('test', $test);
-    }
-
 }
 
 ?>
