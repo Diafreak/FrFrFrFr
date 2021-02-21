1b4c9cf27472380605f5f2a37d26f5639d5dc566
diafr <diafreak212@gmail.com>
2021-02-21 21:34:56 +0100

    moved functions into helper

--- config/init.php
+++ config/init.php
@@ -3,7 +3,7 @@
 define('CONFIGPATH',      'config'     .DIRECTORY_SEPARATOR);
 define('CONTROLLERSPATH', 'controller' .DIRECTORY_SEPARATOR);
 define('COREPATH',        'core'       .DIRECTORY_SEPARATOR);
-define('FUNCTIONSPATH',   'core'       .DIRECTORY_SEPARATOR . 'functions' .DIRECTORY_SEPARATOR);
+define('FUNCTIONSPATH',   'helper'     .DIRECTORY_SEPARATOR . 'functions' .DIRECTORY_SEPARATOR);
 define('IMAGESPATH',      'assets'     .DIRECTORY_SEPARATOR . 'images'    .DIRECTORY_SEPARATOR);
 define('JSPATH',          'assets'     .DIRECTORY_SEPARATOR . 'js'        .DIRECTORY_SEPARATOR);
 define('MODELSPATH',      'models'     .DIRECTORY_SEPARATOR);
