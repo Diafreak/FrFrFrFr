9c998fb70e5218d477aad2821e88d57b1b174f48
DerNoobzockt <leonmause@gmail.com>
2021-02-21 16:05:48 +0100

    erster Homepage versuch

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,13 +1,32 @@
-<h1><?=$test?></h1>
+<div class="homepage">
+    
+    <h1><?=$test?></h1>
+    <!-- "Login Erfolgreich"-Banner -->
+    <div id="success" class="banner login-success">
+        <span>Anmeldung Erfolgreich!</span>
+        <a href="#" class="close">[schließen]</a>
+    </div>
 
-<!-- "Login Erfolgreich"-Banner -->
-<div id="success" class="banner login-success">
-    <span>Anmeldung Erfolgreich!</span>
-    <a href="#" class="close">[schließen]</a>
-</div>
+    <!-- "Erfolgreich Ausgeloggt"-Banner -->
+    <div id="logout" class="banner logout-success">
+        <span>Erfolgreich Ausgeloggt!</span>
+        <a href="#" class="close">[schließen]</a>
+    </div>
 
-<!-- "Erfolgreich Ausgeloggt"-Banner -->
-<div id="logout" class="banner logout-success">
-    <span>Erfolgreich Ausgeloggt!</span>
-    <a href="#" class="close">[schließen]</a>
+    <div class="piclinks">
+        <a href="?c=shop&a=fruits">
+            <img src=<?=IMAGESPATH . 'fruit.jpg'?> alt="fruitlink" class="shoplink fruits">
+        </a>
+        <a href="?c=shop&a=vegetables">
+            <img src=<?=IMAGESPATH . 'vegetables.jpg'?> alt="vegetablelink" class="shoplink vegetables">
+        </a>
+    </div>
+    <div class="welcometext">
+    <p>Willkommen bei Freddie Frettchens Frechen Früchten, DEM Lieferanten für ihren täglichen/wöchentlichen/monatlichen Vitaminbedarf!</p>
+    <p>Wir bieten nur die frischsten und fantastischsten Waren. Überzeugen Sie sich selbst!</p>
+    </div>
+    
+
+    <img src=<?=IMAGESPATH . 'dayumgrape.jpg'?> alt="Willkommensfeld" class="welcoming-field"> 
+
 </div>
