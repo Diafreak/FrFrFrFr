08bbff7c4874da9b9ce1ad07d58888f1c1de0ee5
diafr <diafreak212@gmail.com>
2021-02-21 21:25:01 +0100

    Home Heading

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,6 +1,7 @@
 <div class="homepage">
-    
-    <h1><?=$test?></h1>
+
+<h1>Freddie Frettchens Freche Früchte</h1>
+
     <!-- "Login Erfolgreich"-Banner -->
     <div id="success" class="banner login-success">
         <span>Anmeldung Erfolgreich!</span>
@@ -13,17 +14,19 @@
         <a href="#" class="close">[schließen]</a>
     </div>
 
+
     <div class="piclinks">
         <a href="?c=shop&a=fruits">
-            <img src=<?=IMAGESPATH . 'fruit.jpg'?> alt="fruitlink" class="shoplink fruits">
+            <img src=<?=IMAGESPATH . 'fruit.jpg'?> alt="Obstlink" class="shoplink fruits">
         </a>
         <a href="?c=shop&a=vegetables">
-            <img src=<?=IMAGESPATH . 'vegetables.jpg'?> alt="vegetablelink" class="shoplink vegetables">
+            <img src=<?=IMAGESPATH . 'vegetables.jpg'?> alt="Gemüselink" class="shoplink vegetables">
         </a>
     </div>
+
     <div class="welcometext">
-    <p>Willkommen bei Freddie Frettchens Frechen Früchten, DEM Lieferanten für ihren täglichen/wöchentlichen/monatlichen Vitaminbedarf!</p>
-    <p>Wir bieten nur die frischsten und fantastischsten Waren. Überzeugen Sie sich selbst!</p>
+        <p>Willkommen bei Freddie Frettchens Frechen Früchten, DEM Lieferanten für ihren täglichen/wöchentlichen/monatlichen Vitaminbedarf!</p>
+        <p>Wir bieten nur die frischsten und fantastischsten Waren. Überzeugen Sie sich selbst!</p>
     </div>
     
 
