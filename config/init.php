222eddf310c2786b325f198101c3393a2f96b454
diafr <diafreak212@gmail.com>
2021-02-21 15:10:27 +0100

    added js-path

--- config/init.php
+++ config/init.php
@@ -5,6 +5,7 @@ define('CONTROLLERSPATH', 'controller' .DIRECTORY_SEPARATOR);
 define('COREPATH',        'core'       .DIRECTORY_SEPARATOR);
 define('FUNCTIONSPATH',   'core'       .DIRECTORY_SEPARATOR . 'functions' .DIRECTORY_SEPARATOR);
 define('IMAGESPATH',      'assets'     .DIRECTORY_SEPARATOR . 'images'    .DIRECTORY_SEPARATOR);
+define('JSPATH',          'assets'     .DIRECTORY_SEPARATOR . 'js'        .DIRECTORY_SEPARATOR);
 define('MODELSPATH',      'models'     .DIRECTORY_SEPARATOR);
 define('STYLESPATH',      'assets'     .DIRECTORY_SEPARATOR . 'styles'    .DIRECTORY_SEPARATOR);
 define('VIEWSPATH',       'views'      .DIRECTORY_SEPARATOR);
