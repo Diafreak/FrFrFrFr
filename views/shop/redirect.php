c1701a62fda17a098bc4c213e55b6b4171d1674b
diafr <diafreak212@gmail.com>
2021-02-22 21:31:32 +0100

    redirect page after buying

---
+++ views/shop/redirect.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta http-equiv = "refresh" content = "3; url = index.php" />
+   </head>
+   <body>
+      <p>Kauf wurde erfolgreich abgeschlossen. Sie werden in 5 Sekunden weitergeleitet</p>
+   </body>
+</html>
