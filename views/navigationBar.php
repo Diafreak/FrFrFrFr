9fb95e1c80834b4a828577d78b4f1c80f5fe5247
DerNoobzockt <leonmause@gmail.com>
2021-02-22 21:40:38 +0100

    erste schritte mobile seite

--- views/navigationBar.php
+++ views/navigationBar.php
@@ -1,7 +1,6 @@
 <body>
-    <header>
-        <nav>
-
+    <header>    
+        <nav class="desktop">
             <ul>
                 <li class="no-hover">
                     <a href="?c=pages&a=home" type="no-hover" class="navbar-icon">
@@ -35,7 +34,45 @@
                 </li>
 
             </ul>
+        </nav>
 
+        <nav class="tablet">
+            <div class="navbartrigger">
+                <a href="#nav" class="menubutton">
+                    <img src=<?=IMAGESPATH.'hamburgerbutton.png'?> alt="menu-icon">
+                </a>
+
+                <a href="?c=pages&a=home" type="no-hover" class="navbar-icon">
+                    <img src=<?=IMAGESPATH.'navBar/ferret.svg'?> alt="Logo" class="logo">
+                </a>
+
+                <a href=<?= "{$currentURL}&cart=show" ?> type="no-hover">
+                    <img src=<?=IMAGESPATH.'navBar/basket.svg'?> alt="Warenkorb" class="nav-icon">
+                </a>
+
+                <a href="?c=account&a=login" type="no-hover">
+                    <img src=<?=IMAGESPATH.'navBar/user.svg'?> alt="Login" class="nav-icon">
+                </a>
+            </div>
+            <div id="nav">
+                <ul>
+                    <a href="?c=pages&a=home">
+                        <li>HOME</li>
+                    </a>
+                    <a href="?c=shop&a=fruits">
+                        <li>OBST</li>
+                    </a>
+                    <a href="?c=shop&a=vegetables">
+                        <li>GEMÜSE</li>
+                    </a>
+                    <a href="?c=pages&a=current">
+                        <li>AKTUELLES</li>
+                    </a>
+                    <a href="?c=pages&a=contact">
+                        <li>KONTAKT</li>
+                    </a>
+                </ul>
+            </div>
         </nav>
     </header>
 </body>
