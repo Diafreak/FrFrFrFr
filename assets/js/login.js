c43eb843651894b2065086ae662d7d59d6756e7b
diafr <diafreak212@gmail.com>
2021-02-23 13:18:13 +0100

    cleaned code

--- assets/js/login.js
+++ assets/js/login.js
@@ -31,41 +31,15 @@ document.addEventListener('DOMContentLoaded', function()
 
 
         // check if fields are empty
-        errorSpan.style.display = "block";
-        errorSpan.textContent   = "Alle Felder müssen ausgefüllt sein!";
-        email.className    = "errorinput";
-        password.className = "errorinput";
-
-        if ((email.value    == null || email.value    == "")
-        &&  (password.value == null || password.value == "") )
-        {
-            validInputs = false;
-        }
-        else if (email.value == null || email.value == "")
-        {
-            validInputs = false;
-            password.className = password.className.replace('errorinput', '');
-        }
-        else if (password.value == null || password.value == "")
-        {
-            validInputs = false;
-            email.className = email.className.replace('errorinput', '');
-        }
-        else
-        {
-            errorSpan.textContent   = "";
-            errorSpan.style.display = "none";
-            email.className    = email.className.   replace('errorinput', '');
-            password.className = password.className.replace('errorinput', '');
-        }
+        validInputs = checkForEmptyFields(errorSpan, email, password, validInputs)
 
 
-        // red border for "Login"-Button
         if (!validInputs)
         {
-            document.getElementById('submitLogin').style.border = "1px solid red";
             // hide php errors when showing JS errors
             errorSpanPHP.style.display = "none";
+            // red border for "Login"-Button
+            document.getElementById('submitLogin').style.border = "1px solid red";
         }
         // green border for "Login"-Button
         else
@@ -77,3 +51,46 @@ document.addEventListener('DOMContentLoaded', function()
         return validInputs;
     }
 })
+
+
+
+// =========================================
+// =============== FUNCTIONS ===============
+// =========================================
+
+function checkForEmptyFields(errorSpan, email, password, validInputs)
+{
+    errorSpan.style.display = "block";
+    errorSpan.textContent   = "Alle Felder müssen ausgefüllt sein!";
+    email.className    = "errorinput";
+    password.className = "errorinput";
+
+    // if both fields are empty
+    if ( (email.value    == null || email.value    == "")
+    &&   (password.value == null || password.value == "") )
+    {
+        validInputs = false;
+    }
+    // if email is empty
+    else if (email.value == null || email.value == "")
+    {
+        validInputs = false;
+        password.className = password.className.replace('errorinput', '');
+    }
+    // if password is empty
+    else if (password.value == null || password.value == "")
+    {
+        validInputs = false;
+        email.className = email.className.replace('errorinput', '');
+    }
+    // if all fields are filled
+    else
+    {
+        errorSpan.textContent   = "";
+        errorSpan.style.display = "none";
+        email.className    = email.className.   replace('errorinput', '');
+        password.className = password.className.replace('errorinput', '');
+    }
+
+    return validInputs;
+}
